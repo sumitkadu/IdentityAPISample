8b8a8e54b497adf3772eafd54a54400a0c3dbb3f
Sumit Kadu <sumit@theapplabb.com>
2023-11-15 14:42:35 -0500

    initial commit

---
+++ MyNetCoreAPI/MyNetCoreAPI/Auth/Response.cs
@@ -0,0 +1,8 @@
+﻿namespace MyNetCoreAPI.Auth
+{
+    public class Response
+    {
+        public string? Status { get; set; }
+        public string? Message { get; set; }
+    }
+}
---
+++ MyNetCoreAPI/MyNetCoreAPI/Controllers/WeatherForecastController.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using MyNetCoreAPI.Auth;
+
+namespace MyNetCoreAPI.Controllers
+{
+    [Authorize(Roles =UserRoles.Admin)]
+    [Route("api/[controller]")]
+    [ApiController]
+    public class WeatherForecastController : ControllerBase
+    {
+        static string[] summaries = new[]
+        {
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        };
+
+        [HttpGet]
+        [Route("weatherforecast")]
+        public List<WeatherForecast> GetWeatherForecast()
+        {
+            var forecast = Enumerable.Range(1, 5).Select(index =>
+                new WeatherForecast
+                (
+                    DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
+                    Random.Shared.Next(-20, 55),
+                    summaries[Random.Shared.Next(summaries.Length)]
+                ))
+                .ToList();
+            return forecast;
+        }
+    }
+
+    public class WeatherForecast
+    {
+        public DateOnly Date { get; set; }
+        public int TemperatureC { get; set; }
+        public string? Summary { get; set; }
+        public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+
+            public WeatherForecast(DateOnly date, int temperature, string summary)
+            {
+                this.Date = date;
+                this.TemperatureC = temperature;
+                this.Summary = summary;
+            }
+    }
+}
